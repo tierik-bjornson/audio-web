d3fe2df5448694e79d3cb4f25bfab5e6c7eb541f
tierik-bjornson <95566829+tierik-bjornson@users.noreply.github.com>
2025-03-06 01:32:04 +0700

    Update NavLinks.jsx

--- src/component/NavLinks.jsx
+++ src/component/NavLinks.jsx
@@ -4,10 +4,10 @@ import {Link} from 'react-router-dom'
 import { motion } from 'framer-motion'
 
 const nav_list = [
-  {id:1, name:"Home", navigate:"/"},
-  {id:2, name:"Headphones", navigate:"/HeadPhones"},
-  {id:3, name:"Speakers", navigate:"/Speakers"},
-  {id:4, name:"Earphones", navigate:"/EarPhones"}
+  {id:1, name:"HomePage", navigate:"/"},
+  {id:2, name:"HeadphonesPage", navigate:"/HeadPhones"},
+  {id:3, name:"SpeakersPage", navigate:"/Speakers"},
+  {id:4, name:"EarphonesPage", navigate:"/EarPhones"}
 ]
 
 const NavLinks = ({animation}) => {
